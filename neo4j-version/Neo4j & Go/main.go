9e62a082ca95bc4dee4810b32f264fb96b4ffe7d
AlexFZR <abadragan@gmail.com>
2023-04-13 21:03:38 +0200

    challenge initial commit

--- neo4j-version/Neo4j & Go/main.go
+++ neo4j-version/Neo4j & Go/main.go
@@ -1,55 +1,33 @@
 package main
 
+// Import the driver ("github.com/neo4j/neo4j-go-driver/v5/neo4j")
 import (
 	"context"
-	"fmt"
-	"time"
 
 	. "github.com/neo4j-graphacademy/neoflix/pkg/shared"
-	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
 )
 
 func main() {
-	// Create a Driver Instance
+	// Neo4j Credentials
+	credentials := GetNeo4jCredentials()
 	ctx := context.Background()
-	driver, err := neo4j.NewDriverWithContext(
-		"neo4j+s://dbhash.databases.neo4j.io",    // (1)
-		neo4j.BasicAuth("neo4j", "letmein!", ""), // (2)
-	)
 
-	// Verify Connectivity
-	PanicOnErr(driver.VerifyConnectivity(ctx))
+	// Cypher Query and Parameters
+	cypher := `
+      MATCH (p:Person)-[:DIRECTED]->(:Movie {title: $title})
+      RETURN p.name AS Director
+    `
+	params := map[string]any{"title": "Toy Story"}
 
-	// Open a new Session
-	session := driver.NewSession(ctx, neo4j.SessionConfig{})
+	// TODO: Create a DriverWithContext Instance
 
-	defer PanicOnClosureError(ctx, driver)
-	defer PanicOnClosureError(ctx, session)
+	// TODO: close the driver with defer PanicOnClosureError(ctx, driver)
 
-	// Execute a Cypher statement in an auto-commit transaction
-	result, err := session.Run(
-		ctx, // (1)
-		`
-	MATCH (p:Person)-[:DIRECTED]->(:Movie {title: $title})
-	RETURN p
-	`, // (2)
-		map[string]any{"title": "The Matrix"}, // (3)
-		func(txConfig *neo4j.TransactionConfig) {
-			txConfig.Timeout = 3 * time.Second // (4)
-		},
-	)
-	PanicOnErr(err)
+	// TODO: Open a new Session
 
-	// Get all Person nodes
-	people, err := neo4j.CollectTWithContext[neo4j.Node](ctx, result,
-		func(record *neo4j.Record) (neo4j.Node, error) {
-			person, _, err := neo4j.GetRecordValue[neo4j.Node](record, "p")
-			return person, err
-		})
-	PanicOnErr(err)
-	fmt.Println(people)
+	// TODO: close the session with defer PanicOnClosureError(ctx, driver)
 
-	// Close the driver to release any resources
-	driver.Close(ctx)
+	// TODO: Run a Cypher statement
 
+	// TODO: Log the Director value of the first record
 }
